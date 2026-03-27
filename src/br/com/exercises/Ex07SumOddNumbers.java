617cb9ce0de1ced5e93fa5f59b1c0788b3c2cc85
laryssarodrigues <laryssa.rodrigues@ufrpe.br>
2026-03-27 15:18:47 -0300

    feat: add program to sum odd numbers from 1 to N (number)

---
+++ src/br/com/exercises/Ex07SumOddNumbers.java
@@ -0,0 +1,29 @@
+//program that reads a positive integer N and displays the sum of all odd numbers from 1 to N.
+
+package br.com.exercises;
+
+import java.util.Scanner;
+
+public class Ex07SumOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int cont = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 1) { //verifica se cada numero da vez (i) é ímpar
+                if (i == number || i == (number - 1)) { //se number for o ultimo ímpar (N = ímpar) ou number - 1 (N = par)
+                    System.out.print(i);
+                } else {
+                    System.out.print(i + " + ");
+                }
+                cont += i; //soma os numeros ímpares
+            }
+        }
+
+        System.out.printf(" = %d", cont);
+        sc.close();
+    }
+}

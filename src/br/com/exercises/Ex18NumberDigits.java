6c5612c04d1a47123cc10b1f3943962aada3ca67
laryssarodrigues <laryssa.rodrigues@ufrpe.br>
2026-03-29 01:08:38 -0300

    feat: count number of digits in a positive integer

---
+++ src/br/com/exercises/Ex18NumberDigits.java
@@ -0,0 +1,28 @@
+//program that reads a positive integer and counts how many digits it has.
+
+package br.com.exercises;
+
+import java.util.Scanner;
+
+public class Ex18NumberDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number < 0) {
+            System.out.print("Invalid value!");
+        } else if (number == 0) {
+            System.out.println("Number of digits: 1");
+        } else {
+            int cont = 0;
+            while (number > 0) {
+                number = number / 10;
+                cont++;
+            }
+            System.out.printf("Number of digits: %d.", cont);
+        }
+        sc.close();
+    }
+}

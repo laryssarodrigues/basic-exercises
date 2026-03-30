b8b8e363be4a66fd7a63038c3f5c2cbc3bf6d009
laryssarodrigues <laryssa.rodrigues@ufrpe.br>
2026-03-30 20:20:50 -0300

    feat: count occurrences of letter 'a' in text

---
+++ src/br/com/exercises/Ex23AmountOfA.java
@@ -0,0 +1,23 @@
+//reads a line and counts how many times 'a' or 'A' appears
+
+package br.com.exercises;
+
+import java.util.Scanner;
+
+public class Ex23AmountOfA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type a line: ");
+        String line = sc.nextLine().toLowerCase();
+
+        int counter = 0;
+        for (int i = 0; i < line.length(); i++) {
+            char letter = line.charAt(i);
+            if (letter == 'a') {
+                counter++;
+            }
+        }
+        System.out.printf("aparece %d vezes", counter);
+    }
+}
